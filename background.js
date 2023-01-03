f3eaf8c4e72feae27a8d73cbc407564ecdf2d448
Aaron Fischer <amfischer21@gmail.com>
2023-01-03 13:00:05 -0500

    only use 1 hotkey

--- background.js
+++ background.js
@@ -1,28 +1,27 @@
 const migracionesUrl = 'https://agenciavirtual.migraciones.gob.pe/agencia-virtual/identidad'
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if(request.message === 'open migraciones website'){
-    chrome.tabs.create({url: migracionesUrl, active: true});
-  }
-})
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if(request.message === 'open migraciones website'){
+//     chrome.tabs.create({url: migracionesUrl, active: true});
+//   }
+// })
 
 chrome.commands.onCommand.addListener(async (command) => {
   console.log(`Command "${command}" triggered`);
 
-  if(command === 'open-migraciones-page') {
-    chrome.tabs.create({url: migracionesUrl, active: true});
+  if (command !== 'migraciones-page-interaction') {
+    return;
   }
 
-  if(command === 'page-interaction') {
-    const tab = await getCurrentTab();
-    
-    if(tab === undefined || tab.url.includes('https://agenciavirtual.migraciones.gob.pe') === false){
-      return;
-    }
-
-    chrome.tabs.sendMessage(tab.id, {message: 'do some magic'})
-  }
+  const tab = await getCurrentTab();
   
+  if(tab === undefined || tab.url.includes('https://agenciavirtual.migraciones.gob.pe') === false){
+    chrome.tabs.create({url: migracionesUrl, active: true});
+    return;
+  }
+
+  chrome.tabs.sendMessage(tab.id, {message: 'do some magic'})
+
 });
 
 async function getCurrentTab() {
